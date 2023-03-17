1a19a6941165d7bf5cce4d8b0eba82debe9abbb0
mostfa <myreal17@gmail.com>
2023-03-17 18:02:16 +0100

    update about

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -11,23 +11,13 @@ export default {
 				{
 					id: 2,
 					bio:
-						'Senior Web Developer specializing in front end development. Experienced with all stages of the development cycle for dynamic web projects. Well-versed in numerous programming languages including HTML5, PHP OOP, JavaScript, CSS, MySQL. Strong background in project management and customer relations.',
+						'Senior Web Developer specializing in front end development. Experienced with all stages of the development cycle for dynamic web projects.',
 				},
 				{
 					id: 3,
 					bio:
-						'Senior Web Developer specializing in front end development. Experienced with all stages of the development cycle for dynamic web projects. Well-versed in numerous programming languages including HTML5, PHP OOP, JavaScript, CSS, MySQL. Strong background in project management and customer relations.',
+						'I\'m Well-versed in numerous programming languages including HTML5, PHP OOP, JavaScript, CSS, MySQL. Strong background in project management and customer relations.',
 				},
-				// {
-				// 	id: 2,
-				// 	bio:
-				// 		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-				// },
-				// {
-				// 	id: 3,
-				// 	bio:
-				// 		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-				// },
 			],
 		};
 	},
@@ -39,7 +29,7 @@ export default {
 		<!-- About profile image -->
 		<div class="w-full sm:w-1/4 mb-7 sm:mb-0">
 			<img
-				src="https://avatars.githubusercontent.com/u/17887972?v=4"
+				src="@/assets/images/me2.jpg"
 				class="rounded-xl w-96"
 				alt=""
 			/>
