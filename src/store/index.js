e31c64dba6ffc4fe6e45fafed0b3ba8898adb5c4
mostfa <myreal17@gmail.com>
2023-07-30 02:34:55 +0100

    update cv + fix reload SingleProject

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,16 @@
 import Vuex from 'vuex'
 
 // Vue.use(Vuex)
-
+const initialPageId = localStorage.getItem('pageid') || 0;
 export default new Vuex.Store({
   state: {
-    pageid: 0
+    pageid: parseInt(initialPageId)
   },
   mutations: {
     setPageId(state, id) {
       state.pageid = id;
+      // Save the updated pageid to LocalStorage
+      localStorage.setItem('pageid', id);
     }
   },
   actions: {
