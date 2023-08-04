ff34cafe89aea08af821d27d6c25e51b442cfb37
mostfa <myreal17@gmail.com>
2023-08-04 19:40:23 +0100

    brands position

--- src/components/about/AboutClientSingle.vue
+++ src/components/about/AboutClientSingle.vue
@@ -8,7 +8,7 @@ export default {
 	<div>
 		<a :href="client.link" target="_blank" rel="noopener noreferrer">
 			<img :src="client.img" :alt="client.title"
-				class="w-64 py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer dark:bg-secondary-light" />
+				class="py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer dark:bg-secondary-light" />
 		</a>
 	</div>
 </template>
