d331838e40dafc4acb4dce418ec9d9fbfe5ba4ba
mostfa <myreal17@gmail.com>
2023-08-04 19:24:11 +0100

    emp-history

--- src/components/about/AboutEmplymentHistory.vue
+++ src/components/about/AboutEmplymentHistory.vue
@@ -24,21 +24,29 @@ export default {
 
 
 <template>
-    <div class="container mx-auto p-4 mt-5 text-primary-dark dark:text-primary-light">
-        <h1 class="text-2xl font-semibold m-3">{{ $t('Employment History') }}</h1>
-        <div v-for="(job, index) in employmentHistory" :key="index" class="flex  flex-col my-12 container ">
-            <div class="p-4 mb-4 mx-96 rounded border border-ternary-light dark:border-ternary-dark">
-                <div class="rounded  flex flex-row justify-between items-center ">                    
-                    <img class="w-24 py-3 px-2 border shadow-sm rounded-lg m-1 cursor-pointer dark:bg-secondary-light"
-                    :src="job.logo" alt="logo" />
-                    <div class="flex-initial w-64">
-                        <h2 class="text-xl font-semibold">{{ job.title }}</h2>
-                        <p class="dark:text-gray-200">{{ job.company }}</p>
-                        <p class="dark:text-gray-200">{{ job.startDate }} - {{ job.endDate }}</p>
+    <div class=" mb-8 ">
+        <div class="p-4 mt-5 text-primary-dark dark:text-primary-light flex flex-wrap justify-center">
+            <h1 class="w-full text-2xl font-semibold mb-3">{{ $t('Employment History') }}</h1>
+        </div>
+        <div class="flex justify-center">
+            <div class="flex justify-center items-stretch space-x-4">
+                <!-- Assuming employmentHistory contains exactly 3 elements -->
+                <div v-for="(job, index) in employmentHistory" :key="index" class="max-w-[30%] flex-1 dark:text-gray-200">
+                    <div class="p-8 mb-4 rounded border border-ternary-light dark:border-ternary-dark h-full">
+
+                        <div class="flex justify-between items-center">
+                            <img class="w-24 py-3 px-2 border shadow-sm rounded-lg m-1 cursor-pointer dark:bg-secondary-light"
+                                :src="job.logo" alt="logo" />
+                            <div class="flex-1">
+                                <h2 class="text-xl font-semibold">{{ job.title }}</h2>
+                                <p class="">{{ job.company }}</p>
+                                <p class="">{{ job.startDate }} - {{ job.endDate }}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="mt-8 text-center">{{ job.description }}</p>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <p class="mt-8 text-center">{{ job.description }}</p>
                 </div>
             </div>
         </div>
