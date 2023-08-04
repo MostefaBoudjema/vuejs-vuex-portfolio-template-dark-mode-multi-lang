26701b98a678127f3574369e52a72052d54858a8
mostfa <myreal17@gmail.com>
2023-08-04 18:53:17 +0100

    emp history style

--- src/components/about/AboutEmplymentHistory.vue
+++ src/components/about/AboutEmplymentHistory.vue
@@ -10,12 +10,12 @@ export default {
         };
     },
     setup() {
-        const { t } = useI18n({
+        const { t }=useI18n({
             inheritLocale: true,
             useScope: "local",
         });
 
-        
+
 
         return { t };
     },
@@ -25,12 +25,22 @@ export default {
 
 <template>
     <div class="container mx-auto p-4 mt-5 text-primary-dark dark:text-primary-light">
-        <h1 class="text-2xl font-semibold mb-4">{{$t('Employment History')}}</h1>
-        <div v-for="(job, index) in employmentHistory" :key="index" class="rounded p-4 mb-4">
-            <h2 class="text-xl font-semibold">{{ job.title }}</h2>
-            <p class="text-gray-600">{{ job.company }}</p>
-            <p class="text-gray-600">{{ job.startDate }} - {{ job.endDate }}</p>
-            <p class="mt-2">{{ job.description }}</p>
+        <h1 class="text-2xl font-semibold m-3">{{ $t('Employment History') }}</h1>
+        <div v-for="(job, index) in employmentHistory" :key="index" class="flex  flex-col my-12 container ">
+            <div class="p-4 mb-4 mx-96 rounded border border-ternary-light dark:border-ternary-dark">
+                <div class="rounded  flex flex-row justify-between items-center ">                    
+                    <img class="w-24 py-3 px-2 border shadow-sm rounded-lg m-1 cursor-pointer dark:bg-secondary-light"
+                    :src="job.logo" alt="logo" />
+                    <div class="flex-initial w-64">
+                        <h2 class="text-xl font-semibold">{{ job.title }}</h2>
+                        <p class="dark:text-gray-200">{{ job.company }}</p>
+                        <p class="dark:text-gray-200">{{ job.startDate }} - {{ job.endDate }}</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="mt-8 text-center">{{ job.description }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- src/data/EmplymentHistory.js
+++ src/data/EmplymentHistory.js
@@ -1,12 +1,13 @@
 import i18n from '../i18n';
-const { t } = i18n.global;
+const { t }=i18n.global;
 
-const EmplymentHistory = [
+const EmplymentHistory=[
     {
         title: t("JobTitle1"),
         company: t("JobCompany1"),
         startDate: t("JobStartDate1"),
         endDate: t("JobEndDate1"),
+        logo: require('@/assets/images/brands/mesnja_color.png'),
         description: t("JobDescription1")
     },
     {
@@ -14,6 +15,7 @@ const EmplymentHistory = [
         company: t("JobCompany2"),
         startDate: t("JobStartDate2"),
         endDate: t("JobEndDate2"),
+        logo: require('@/assets/images/brands/upwork_color.png'),
         description: t("JobDescription2")
     },
     {
@@ -21,6 +23,7 @@ const EmplymentHistory = [
         company: t("JobCompany3"),
         startDate: t("JobStartDate3"),
         endDate: t("JobEndDate3"),
+        logo: require('@/assets/images/brands/biginformatique_color.png'),
         description: t("JobDescription3")
     },
 ];
