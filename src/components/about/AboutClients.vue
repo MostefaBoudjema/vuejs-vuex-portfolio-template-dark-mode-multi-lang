bca855112d965e7f5246c028b68f5a3e4426c74c
mostfa <myreal17@gmail.com>
2023-08-04 19:36:29 +0100

    last fix

--- src/components/about/AboutClients.vue
+++ src/components/about/AboutClients.vue
@@ -26,7 +26,7 @@ export default {
         >
             {{ clientsHeading }}
         </p>
-        <div class="grid grid-cols-2 sm:grid-cols-4 mt-10 sm:mt-14 gap-2">
+        <div class="grid grid-cols-1 sm:grid-cols-4 mt-10 sm:mt-14 gap-2">
             <AboutClientSingle
                 v-for="client in clients"
                 :key="client.id"
--- src/components/about/AboutEmplymentHistory.vue
+++ src/components/about/AboutEmplymentHistory.vue
@@ -24,32 +24,31 @@ export default {
 
 
 <template>
-    <div class=" mb-8 ">
-        <div class="p-4 mt-5 text-primary-dark dark:text-primary-light flex flex-wrap justify-center">
-            <h1 class="w-full text-2xl font-semibold mb-3">{{ $t('Employment History') }}</h1>
-        </div>
-        <div class="flex justify-center">
-            <div class="flex justify-center items-stretch space-x-4">
-                <!-- Assuming employmentHistory contains exactly 3 elements -->
-                <div v-for="(job, index) in employmentHistory" :key="index" class="max-w-[30%] flex-1 dark:text-gray-200">
-                    <div class="p-8 mb-4 rounded border border-ternary-light dark:border-ternary-dark h-full">
-
-                        <div class="flex justify-between items-center">
-                            <img class="w-24 py-3 px-2 border shadow-sm rounded-lg m-1 cursor-pointer dark:bg-secondary-light"
-                                :src="job.logo" alt="logo" />
-                            <div class="flex-1">
-                                <h2 class="text-xl font-semibold">{{ job.title }}</h2>
-                                <p class="">{{ job.company }}</p>
-                                <p class="">{{ job.startDate }} - {{ job.endDate }}</p>
-                            </div>
-                        </div>
-                        <div>
-                            <p class="mt-8 text-center">{{ job.description }}</p>
-                        </div>
-                    </div>
+<div class="mb-8">
+    <div class="p-4 mt-5 text-primary-dark dark:text-primary-light flex flex-wrap justify-center">
+        <h1 class="w-full text-2xl font-semibold mb-3">{{ $t('Employment History') }}</h1>
+    </div>
+    <div class="flex justify-center flex-wrap">
+    <div v-for="(job, index) in employmentHistory" :key="index"
+         class="w-full md:w-[30%] dark:text-gray-200 mb-4 md:mb-0 md:mr-4">
+        <div class="p-8 rounded sm:border border-ternary-light dark:border-ternary-dark h-full">
+            <div class="flex justify-center md:justify-between items-center">
+                <img class="w-24 py-3 px-2 border shadow-sm rounded-lg m-1 cursor-pointer dark:bg-secondary-light"
+                    :src="job.logo" alt="logo" />
+                <div class="flex-1 md:ml-4 mt-4 md:mt-0">
+                    <h2 class="text-xl font-semibold">{{ job.title }}</h2>
+                    <p class="">{{ job.company }}</p>
+                    <p class="">{{ job.startDate }} - {{ job.endDate }}</p>
                 </div>
+            </div>
+            <div>
+                <p class="mt-8 text-center">{{ job.description }}</p>
             </div>
         </div>
     </div>
+</div>
+
+</div>
+
 </template>
   
