e773fa8ac7ffb9ca2fa1cccb487c4a2fb1baddd2
mostfa <myreal17@gmail.com>
2023-09-30 17:26:16 +0100

    remove GA

--- src/main.js
+++ src/main.js
@@ -7,30 +7,31 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import BackToTop from 'vue-backtotop';
 import i18n from './i18n';
 
-import VueAnalytics from 'vue-analytics';
+// import VueAnalytics from 'vue-analytics';
 
-const feather = require('feather-icons');
+const feather=require('feather-icons');
 feather.replace();
 
-const app = createApp(App);
+const app=createApp(App);
 app.use(i18n)
 	.use(router)
 	.use(store)
 	.use(BackToTop)
-	.use(VueAnalytics, {
-		id: 'G-459NGJ4ZD7', // Replace with your Google Analytics tracking code
-		router
-	  });
+	// .use(VueAnalytics, {
+	// 	id: 'G-459NGJ4ZD7', 
+	// 	router
+	// })
+	;
 
-app.config.devtools = true;
-window.i18n = i18n;
+app.config.devtools=true;
+window.i18n=i18n;
 app.mount('#app');
 
-const appTheme = localStorage.getItem('theme');
+const appTheme=localStorage.getItem('theme');
 
 // Check what is the active theme and change theme when user clicks on the theme button in header.
 if (
-	appTheme === 'dark' &&
+	appTheme==='dark'&&
 	document.querySelector('body').classList.contains('app-theme')
 ) {
 	document.querySelector('body').classList.add('bg-primary-dark');
