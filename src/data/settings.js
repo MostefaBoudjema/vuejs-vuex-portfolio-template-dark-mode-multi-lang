cdff1b6e9b616205ab63ca2f231f2de9b83fa977
mostfa <myreal17@gmail.com>
2024-01-23 16:18:39 +0100

    update whatss

--- src/data/settings.js
+++ src/data/settings.js
@@ -7,7 +7,7 @@ const settings=
 	logo_light: require("@/assets/images/logo-light.svg"),
 	page_404: require("@/assets/images/404-page.png"),
 	profile_photo: require("@/assets/images/me2.jpg"),
-	whatsapp: require("@/assets/images/whatsapp.svg"),
+	whatsapp: require("@/assets/images/whatsapp2.svg"),
 	whatsappNumber: "213793692289",
 	full_list: [3, 9, 1, 10, 0, 11, 5, 2, 4, 6, 7, 8],
 	home_list: [3, 9, 1, 10, 0, 11],
