40654e46677f2a297a579f6f15a49615bf0477b5
mostfa <myreal17@gmail.com>
2025-04-06 23:29:36 +0100

    add home in header

--- src/components/shared/AppHeaderLinks.vue
+++ src/components/shared/AppHeaderLinks.vue
@@ -24,8 +24,14 @@ export default {
         class="m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none"
     >
         <router-link
-            :to="{ name: 'Projects' }"
+            :to="{ name: 'Home' }"
             class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-blue-600 dark:hover:text-blue-300 sm:mx-4 mb-2 sm:py-2"
+            aria-label="Home"
+            >{{ t("Home") }}</router-link
+        >
+        <router-link
+            :to="{ name: 'Projects' }"
+            class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-blue-600 dark:hover:text-blue-300 sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
             aria-label="Projects"
             >{{ t("Projects") }}</router-link
         >
