36d3a6b20e05639ef1685a5213fe39ba2a5936d2
mostfa <myreal17@gmail.com>
2025-06-16 17:37:08 +0100

    fix bug

---
+++ src/components/about/AboutVideo.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="flex justify-center mt-10 sm:mt-20">
+		<iframe
+			width="560"
+			height="315"
+			:src="settings.videoLinkEn"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+		></iframe>
+	</div>
+</template>
+
+
+<script setup>
+import i18n from '@/i18n';
+import settings from '@/data/settings';
+
+const { t }=i18n.global;
+
+const bios=[
+	{ id: 1, bio: t('AboutMe1') },
+	{ id: 2, bio: t('AboutMe2') },
+	{ id: 3, bio: t('AboutMe3') },
+	{ id: 4, bio: t('AboutMe4') },
+	{ id: 5, bio: t('AboutMe5') },
+];
+</script>
+
+<style scoped></style>
--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -1,55 +1,60 @@
 <template>
-    <section class="flex flex-col sm:justify-between items-center sm:flex-row mt-12 sm:mt-10">
-      <!-- Banner left contents -->
-      <div class="w-full md:w-1/3 text-left" data-aos="fade-right" data-aos-delay="200">
-        <h1 class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase">
-          {{ t("Hi, Iam Mostfa") }}
-        </h1>
-        <p class="font-general-medium mt-2 text-lg sm:text-xl xl:text-2xl text-center sm:text-left leading-none text-gray-400">
-          {{ t("Job Title") }}
-        </p>
-        <DownloadCv />
+  <section class="flex flex-col sm:justify-between items-center sm:flex-row mt-12 sm:mt-10">
+    <!-- Banner left contents -->
+    <div class="w-full md:w-1/3 text-left" data-aos="fade-right" data-aos-delay="200">
+      <h1
+        class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase">
+        {{ t("Hi, Iam Mostfa") }}
+      </h1>
+      <p
+        class="font-general-medium mt-2 text-lg sm:text-xl xl:text-2xl text-center sm:text-left leading-none text-gray-400">
+        {{ t("Job Title") }}
+      </p>
+      <DownloadCv />
 
-      </div>
-  
-      <!-- Banner right illustration -->
-      <div class="w-full md:w-2/3 text-right float-right">
-        <DotLottieVue style="height: 600px;" autoplay loop src="https://lottie.host/e3d2a44b-7d02-4b19-8d05-e90af78f1571/FGAlWtRxOu.lottie" />
-        <!-- <DotLottieVue style="height: 600px;" autoplay loop src="https://lottie.host/92926995-bda3-46ba-943f-51ae646541df/0E3TNpU72O.lottie" /> -->
-      </div>
-      <!-- <div class="w-full md:w-2/3 text-right float-right">
+      <ContactBtn />
+
+    </div>
+
+    <!-- Banner right illustration -->
+    <div class="w-full md:w-2/3 text-right float-right">
+      <DotLottieVue style="height: 600px;" autoplay loop
+        src="https://lottie.host/e3d2a44b-7d02-4b19-8d05-e90af78f1571/FGAlWtRxOu.lottie" />
+      <!-- <DotLottieVue style="height: 600px;" autoplay loop src="https://lottie.host/92926995-bda3-46ba-943f-51ae646541df/0E3TNpU72O.lottie" /> -->
+    </div>
+    <!-- <div class="w-full md:w-2/3 text-right float-right">
         <img v-if="theme === 'light'" :src="settings.hero_image" alt="Developer" data-aos="fade-right" data-aos-delay="200" />
         <img v-else :src="settings.hero_image_dark" alt="Developer" data-aos="fade-right" data-aos-delay="200" />
       </div> -->
-    </section>
-  </template>
-  
-  <script setup>
-  import { onMounted, onUpdated, ref } from 'vue';
-  import feather from 'feather-icons';
-  import { useI18n } from 'vue-i18n';
-  import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
-  import settings from '@/data/settings';
-  import DownloadCv from './DownloadCv.vue';
-  
-  const { t } = useI18n({
-    inheritLocale: true,
-    useScope: 'local',
-  });
-  
-  const theme = ref(localStorage.getItem('theme') || 'light');
-  const lang = ref(localStorage.getItem('lang') || 'ar');
-  
-  onMounted(() => {
-    feather.replace();
-    theme.value = localStorage.getItem('theme') || 'light';
-    lang.value = localStorage.getItem('lang') || 'ar';
-  });
-  
-  onUpdated(() => {
-    feather.replace();
-  });
-  </script>
-  
-  <style scoped></style>
-  
+  </section>
+</template>
+
+<script setup>
+import { onMounted, onUpdated, ref } from 'vue';
+import feather from 'feather-icons';
+import { useI18n } from 'vue-i18n';
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+import settings from '@/data/settings';
+import DownloadCv from './DownloadCv.vue';
+import ContactBtn from './ContactBtn.vue';
+
+const { t }=useI18n({
+  inheritLocale: true,
+  useScope: 'local',
+});
+
+const theme=ref(localStorage.getItem('theme')||'light');
+const lang=ref(localStorage.getItem('lang')||'ar');
+
+onMounted(() => {
+  feather.replace();
+  theme.value=localStorage.getItem('theme')||'light';
+  lang.value=localStorage.getItem('lang')||'ar';
+});
+
+onUpdated(() => {
+  feather.replace();
+});
+</script>
+
+<style scoped></style>
