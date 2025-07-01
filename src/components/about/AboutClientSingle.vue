3fdfad0fec7cd62b0e80faaa0351251fb352d858
mostfa <myreal17@gmail.com>
2025-07-01 19:19:29 +0100

    fix size of v-lazy

--- src/components/about/AboutClientSingle.vue
+++ src/components/about/AboutClientSingle.vue
@@ -6,16 +6,24 @@ export default {
 
 <template>
 	<div  data-aos="zoom-in-up">
-		<!-- Check if client.link is not empty -->
 		<a v-if="client.link" :href="client.link" target="_blank" rel="noopener noreferrer">
 			<img v-lazy="client.img" :alt="client.title"
 				class="py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer dark:bg-secondary-light" />
 		</a>
-		<!-- If client.link is empty or falsy, just render the image without the anchor tag -->
 		<img v-else v-lazy="client.img" :alt="client.title"
-			class="py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer dark:bg-secondary-light" />
+			width="300"
+			height="160"
+			class="client-img py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer dark:bg-secondary-light"
+		/>
 	</div>
 </template>
   
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.client-img {
+	width: 300px;
+	height: 160px;
+	object-fit: contain;
+	display: block;
+}
+</style>
